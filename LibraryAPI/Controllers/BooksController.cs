2d440777fc63cc3028ea012bd26a579c00b873ac
Raveen <raveenrandika999@gmail.com>
2025-06-14 18:20:37 +0530

    Added Book CRUD operations and tested with JWT Tokens

---
+++ LibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LibraryAPI.Data;
+using LibraryAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace LibraryAPI.Controllers
+{
+
+    [Authorize] // Require authentication for all actions in this controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController :ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public BooksController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        // GET api/books bt id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) return NotFound();
+            return book;
+        }
+
+        // POST api/books
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        // PUT: api/books by id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book book)
+        {
+            if (id != book.Id) return BadRequest();
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Books.Any(b => b.Id == id)) return NotFound();
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/books by id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) return NotFound();
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
